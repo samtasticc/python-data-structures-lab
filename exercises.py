d93ac5b363be36c7f4a58d85ca8fd3061cb02e90
samtasticc <samantha.henesy@gmail.com>
2025-01-19 13:47:07 -0500

    exercise 1 pass

--- exercises.py
+++ exercises.py
@@ -6,11 +6,30 @@
 # Assign the second student’s name to a variable named first_student.
 # Assign the last student’s name to a variable named last_student.
 
-students = ['Eric', 'Dee', 'Sam']
-def manage_students():
+# students = ['Eric', 'Dee', 'Sam']
+# def manage_students():
+#     # your code here
+#     first_student = students[0]
+#     last_student = students[-1]
+#     return (f"First student: {first_student}, Last student: {last_student}")
+# # Call the function and print the result
+# print('Exercise 1:', manage_students())
+
+# ------ EXERCISE 2
+
+# Exercise 2: Loop and String Concatenation
+#
+# Create a tuple named foods containing the same number of foods (strings) as there are names in the students list.
+# Create a variable named meal and assign an empty string to it.
+# Use a for loop to iterate over the strings in foods and append each string to meal.
+
+foods = ('quinoa', 'broccoli', 'english muffins')
+def combine_foods():
     # your code here
-    first_student = students[0]
-    last_student = students[-1]
-    return (f"First student: {first_student}, Last student: {last_student}")
+    meal = ' '
+    for food in foods:
+        meal += food + ' '
+    return meal
+
 # Call the function and print the result
-print('Exercise 1:', manage_students())
+print('Exercise 2:', combine_foods())
