7ce6e5f3527dc5ebea397846624ecd88814c4b72
samtasticc <samantha.henesy@gmail.com>
2025-01-19 14:21:14 -0500

    exercise 4 pass

--- exercises.py
+++ exercises.py
@@ -23,16 +23,16 @@
 # Create a variable named meal and assign an empty string to it.
 # Use a for loop to iterate over the strings in foods and append each string to meal.
 
-foods = ('quinoa', 'broccoli', 'english muffins')
-def combine_foods():
-    # your code here
-    meal = ' '
-    for food in foods:
-        meal += food + ' '
-    return meal
+# foods = ('quinoa', 'broccoli', 'english muffins')
+# def combine_foods():
+#     # your code here
+#     meal = ' '
+#     for food in foods:
+#         meal += food + ' '
+#     return meal
 
-# Call the function and print the result
-print('Exercise 2:', combine_foods())
+# # Call the function and print the result
+# print('Exercise 2:', combine_foods())
 
 
 # ------ EXERCISE 3
@@ -41,10 +41,29 @@ print('Exercise 2:', combine_foods())
 #
 # Using the slice operator, assign a new tuple containing only the last two food strings in the foods to a variable named last_two_foods.
 
-def slice_foods():
-    # your code here
-    last_two_foods = foods[1:3]
-    return last_two_foods
+# def slice_foods():
+#     # your code here
+#     last_two_foods = foods[1:3]
+#     return last_two_foods
 
+# # Call the function and print the result
+# print('Exercise 3:', slice_foods())
+
+# ------ EXERCISE 4
+
+# Exercise 4: Dictionaries and String Formatting
+#
+# Create a dictionary named home_town containing the keys of city, state, and population.
+# Using the home_town dictionary, assign to a variable named home_town_message a string with this format: “I was born in <city>, <state> - population of <population>”
+
+home_town = {
+    'city': 'Clearwater',
+    'state': 'Florida',
+    'population': '116,850'
+}
+def hometown_info():
+    # your code here
+    home_town_message = (f'I was born in {home_town['city']}, {home_town['state']} - population of {home_town['population']}.')
+    return home_town_message
 # Call the function and print the result
-print('Exercise 3:', slice_foods())
+print('Exercise 4:', hometown_info())
